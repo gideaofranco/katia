2e36de81bb3ab7aaa3e17f73f157db2ca0c59dad
Gideão Franco <gideao.franco>
2024-05-05 17:08:42 -0300

    msg

--- copy.py
+++ copy.py
@@ -3,5 +3,5 @@ with open('twitter-100-linhas.csv', 'r') as ficheiro:
     reader = csv.reader(ficheiro)
     for linha in reader:
         mylen=len(linha[7])
-        mycount=linha[7].count(" ")
+        mycount=linha[7].count(" ") + 1
         print(f"tamanho: {mylen}, palavras: {mycount}")
